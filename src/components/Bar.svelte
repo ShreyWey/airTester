e243d1804ef8d9b093039233f96d16dc8f46b95c
siddarth kotthur <kotthursiddarth@gmail.com>
2023-01-28 10:38:40 -0600

    w

---
+++ src/components/Bar.svelte
@@ -0,0 +1,16 @@
+<script>
+	// @ts-nocheck
+
+	let percentage;
+</script>
+
+<div class="bar_graph">
+	<h1>{[percentage]}</h1>
+</div>
+
+<style>
+	.bar_graph {
+		width: 100%;
+		height: 20px;
+	}
+</style>
