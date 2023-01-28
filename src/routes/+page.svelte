79c7d5fb0b64407f007687423f97555169e51256
ShreyWey <shreyansh.chhalotra@gmail.com>
2023-01-28 11:42:49 -0600

    start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>AeroCheck</h1>
+<p>
+Air quality is one of the biggest environmental concerns today. Pollution and industrialization has caused many problems to our bodies, which in turn affect our major components like lungs, skin, and even brain. Air quality affects everyone's life on a daily basis, from small children to elderly population who need to be protected from harmful pollutants at all times. Our team has set out to tackle this ever growing problem with our newest product AeroCheck.
+</p>
+
+
+<style>
+	.body {
+    background-color:  #191919;
+}
+
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Ubuntu', sans-serif;
+    
+}
+
+::-webkit-scrollbar{
+    width: 6px;
+}
+
+::-webkit-scrollbar-track {
+    background: #191919;
+}
+
+::-webkit-scrollbar-thumb {
+    background:#ffb21991;
+    border-radius: 100vw;
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background: #ffb219e8;
+}
+    
+.h1 {
+	font-size: 2 rem;
+	color: linear-gradient(to top, #0856ff 0%, #ffb119 100%);
+}
+</style>
